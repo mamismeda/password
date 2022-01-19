56befc63792d2f9eac49ed41796dafd6b52cea6c
mamismeda <amamismeda@gmail.com>
2022-01-19 21:54:52 +0400

    end of password generator and validation

--- myscript.js
+++ myscript.js
@@ -14,3 +14,56 @@ forma.addEventListener('submit', function (el) {
 
     el.preventDefault();
 });
+
+password.onfocus = function(){
+    document.getElementById("msg").style.display = "block";
+}
+password.onblur = function(){
+    document.getElementById("msg").style.display = "none"
+}
+
+password.onkeyup = function(){
+
+    if (password.value.length >= 8 ) {
+        members.classList.remove("inavlid")
+        members.classList.add("valid");
+    }else{
+        members.classList.remove("valid");
+        members.classList.add("invalid");
+    }
+
+    let upperCaseLetter = /[A-Z]/g;
+
+    if(password.value.match(upperCaseLetter)) {  
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+      } else {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+      }
+
+    let numberMargin = /[0-9]/g;
+    
+    if(password.value.match(numberMargin)){
+        numy.classList.remove("invalid");
+        numy.classList.add("valid");
+    }else {
+        numy.classList.remove("valid");
+        numy.classList.add("invalid");
+    }
+
+    let specSymbol = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+    if (password.value.match(specSymbol)) {
+        specy.classList.remove("invalid");
+        specy.classList.add("valid");
+        passToggle.classList.remove("invalid");
+        passToggle.classList.add("valid");
+    }else{
+        specy.classList.remove("valid");
+        specy.classList.add("invalid");
+        passToggle.classList.remove("valid");
+        passToggle.classList.add("invalid");
+    }
+
+}
